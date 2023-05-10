bc5bc02f9b0a3e9d627c37cb4f44bc7a78987c76
jay bhogayata <jaybhogayata53@gmail.com>
2023-05-10 18:27:31 +0530

    fix 1

--- app/create-snippets/page.jsx
+++ app/create-snippets/page.jsx
@@ -1,12 +1,8 @@
 "use client";
 import Form from "@components/Form";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 function page() {
-  const router = useRouter();
-  const { data: session } = useSession();
   const [snippet, setSnippet] = useState({
     name: "",
     desc: "",
@@ -21,16 +17,12 @@ function page() {
 
   return (
     <div className=" text-white bg-gray-900 h-screen ">
-      {session?.user ? (
-        <Form
-          type={"create"}
-          snippet={snippet}
-          setSnippet={setSnippet}
-          handleSubmit={createSnippet}
-        />
-      ) : (
-        router.push("/")
-      )}
+      <Form
+        type={"create"}
+        snippet={snippet}
+        setSnippet={setSnippet}
+        handleSubmit={createSnippet}
+      />
     </div>
   );
 }
